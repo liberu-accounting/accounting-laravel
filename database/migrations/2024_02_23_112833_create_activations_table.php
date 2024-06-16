83d44a1b8eb85516352b88bc1bc70570c6b4cd5e
Curtis Delicata <curtis.delicata@outlook.com>
2024-06-16 01:09:40 +0100

    Update 2024_02_23_112833_create_activations_table.php

--- database/migrations/2024_02_23_112833_create_activations_table.php
+++ database/migrations/2024_02_23_112833_create_activations_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->ipAddress('ip_address')->nullable();
             $table->timestamps();
 
-            $table->foreign('user_id')->references('user_id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
