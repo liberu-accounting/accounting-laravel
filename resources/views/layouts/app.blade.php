7300800f11b1ff151c7a430126dc6fdb8595e1eb
Raza9798 <m.raza9798@gmail.com>
2024-09-03 02:59:18 +0530

    liveware refectored

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ \App\Helpers\SiteSettingsHelper::get('name') }}</title>
+    <title>{{ SiteConfig::get('name') }}</title>
 
     @if(config('googletagmanager.id'))
         @include('googletagmanager::head')
