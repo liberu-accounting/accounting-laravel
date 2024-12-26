123e51cdfb3ed1830b1643d96389820d63c71680
Curtis Delicata <curtis.delicata@outlook.com>
2024-12-26 17:39:17 +0000

    Update RoleResource.php

--- app/Filament/Resources/RoleResource.php
+++ app/Filament/Resources/RoleResource.php
@@ -4,9 +4,9 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\RoleResource\Pages;
 use Filament\Forms;
-use Filament\Resources\Form;
+use Filament\Forms\Form;
 use Filament\Resources\Resource;
-use Filament\Resources\Table;
+use Filament\Tables\Table;
 use Filament\Tables;
 use Spatie\Permission\Models\Role;
 
