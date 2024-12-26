5574c757187b67d5e5ae2e34637273cf393503c0
Curtis Delicata <curtis.delicata@outlook.com>
2024-12-26 11:20:34 +0000

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,32 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
-
-class AppServiceProvider extends ServiceProvider
-{
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        //
-    }
-}
-
-
-<?php
-
-namespace App\Providers;
-
 use App\Models\Transaction;
 use App\Observers\TransactionObserver;
 use Illuminate\Support\ServiceProvider;
