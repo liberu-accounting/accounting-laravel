579312dce211afc26f13a88c1ca26469fb49463d
Curtis Delicata <curtis.delicata@outlook.com>
2024-12-26 18:15:42 +0000

    Update ListTransactions.php

--- app/Filament/App/Resources/TransactionResource/Pages/ListTransactions.php
+++ app/Filament/App/Resources/TransactionResource/Pages/ListTransactions.php
@@ -17,24 +17,3 @@ class ListTransactions extends ListRecords
         ];
     }
 }
-
-
-<?php
-
-namespace App\Filament\App\Resources\TransactionResource\Pages;
-
-use App\Filament\App\Resources\TransactionResource;
-use Filament\Actions;
-use Filament\Resources\Pages\ListRecords;
-
-class ListTransactions extends ListRecords
-{
-    protected static string $resource = TransactionResource::class;
-
-    protected function getHeaderActions(): array
-    {
-        return [
-            Actions\CreateAction::make(),
-        ];
-    }
-}
