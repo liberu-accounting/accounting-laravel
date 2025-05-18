508b1467fdfa04bc2fca277486848b063aaf0af8
Curtis Delicata <curtis.delicata@outlook.com>
2025-05-18 21:59:32 +0100

    Update 2024_01_09_112914_create_suppliers_table.php

--- database/migrations/2024_01_09_112914_create_suppliers_table.php
+++ database/migrations/2024_01_09_112914_create_suppliers_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('suppliers', function (Blueprint $table) {
-            $table->integer('supplier_id', true);
+            $table->unsignedBigInteger('supplier_id', true);
             $table->unsignedBigInteger('payment_term_id');
             $table->string('supplier_first_name');
             $table->string('supplier_last_name');
