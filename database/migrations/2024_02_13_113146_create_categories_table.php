006fcc7d5e1c300832ec29beba30cb2cbf4018ca
Curtis Delicata <curtis.delicata@outlook.com>
2025-05-19 21:39:33 +0100

    Update 2024_02_13_113146_create_categories_table.php

--- database/migrations/2024_02_13_113146_create_categories_table.php
+++ database/migrations/2024_02_13_113146_create_categories_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('categories', function (Blueprint $table) {
-            $table->integer('category_id', true);
+            $table->id();
             $table->string('name');
             $table->timestamps();
         });
