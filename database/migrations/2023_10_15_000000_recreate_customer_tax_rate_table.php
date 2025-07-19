c3e1b931aef7cdc4eb19b30b5572351e6cc0b24e
Raza9798 <m.raza9798@gmail.com>
2025-07-20 01:26:02 +0530

    migration update

--- database/migrations/2023_10_15_000000_recreate_customer_tax_rate_table.php
+++ database/migrations/2023_10_15_000000_recreate_customer_tax_rate_table.php
@@ -8,31 +8,31 @@ class RecreateCustomerTaxRateTable extends Migration
 {
     public function up()
     {
-        if (!Schema::hasTable('customers')) {
-            throw new \Exception('The customers table must exist before running this migration.');
-        }
-
         Schema::dropIfExists('customer_tax_rate');
 
         Schema::create('customer_tax_rate', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('customer_id');
-            $table->unsignedBigInteger('tax_rate_id');
+            $table->unsignedBigInteger('customer_id')->nullable();
+            $table->unsignedBigInteger('tax_rate_id')->nullable();
             $table->timestamps();
+        });
 
-            if (Schema::hasTable('customers')) {
+        if (Schema::hasTable('customers')) {
+            Schema::table('customer_tax_rate', function (Blueprint $table) {
                 $table->foreign('customer_id')
                     ->references('id')
                     ->on('customers')
                     ->onDelete('cascade');
-            }
-            
-            if (Schema::hasTable('tax_rates')) {
+            });
+        }
+        
+        if (Schema::hasTable('tax_rates')) {
+            Schema::table('customer_tax_rate', function (Blueprint $table) {
                 $table->foreign('tax_rate_id')
                     ->references('tax_rate_id')
                     ->on('tax_rates');
-            }
-        });
+            });
+        }
     }
 
     public function down()
