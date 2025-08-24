5a6a49161a509cfa662598fcc4bda980da239a56
Curtis Delicata <curtis.delicata@liberu.co.uk>
2025-08-24 14:02:53 +0000

    Update dependencies and upgrade directory structure

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -87,6 +87,13 @@
       0 => 'Filament\\Tables\\TablesServiceProvider',
     ),
   ),
+  'filament/upgrade' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Filament\\Upgrade\\UpgradeServiceProvider',
+    ),
+  ),
   'filament/widgets' => 
   array (
     'providers' => 
