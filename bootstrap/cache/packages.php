b6c4d07a371519153f3d2ebd9901f74c3781c508
Curtis Delicata <curtis.delicat@outlook.com>
2026-03-20 14:50:25 +0000

    Update dependencies

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -1,11 +1,4 @@
 <?php return array (
-  'anourvalar/eloquent-serialize' => 
-  array (
-    'aliases' => 
-    array (
-      'EloquentSerialize' => 'AnourValar\\EloquentSerialize\\Facades\\EloquentSerializeFacade',
-    ),
-  ),
   'bezhansalleh/filament-plugin-essentials' => 
   array (
     'providers' => 
@@ -223,17 +216,6 @@
     'providers' => 
     array (
       0 => 'Termwind\\Laravel\\TermwindServiceProvider',
-    ),
-  ),
-  'ryangjchandler/blade-capture-directive' => 
-  array (
-    'aliases' => 
-    array (
-      'BladeCaptureDirective' => 'RyanChandler\\BladeCaptureDirective\\Facades\\BladeCaptureDirective',
-    ),
-    'providers' => 
-    array (
-      0 => 'RyanChandler\\BladeCaptureDirective\\BladeCaptureDirectiveServiceProvider',
     ),
   ),
   'spatie/laravel-ignition' => 
