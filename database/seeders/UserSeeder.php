33b7d2ccf8fb92dd08bd3a79444a636f64e2c154
Curtis Delicata <curtis.delicata@outlook.com>
2026-03-20 14:32:02 +0000

    Update UserSeeder to use random admin password
    Generate a random password for the admin user and assign the 'super_admin' role. Also, sync the admin user with an existing team.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use App\Models\Team;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -15,22 +16,22 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+
+        $adminPassword = Str::random(12);
         $adminUser = User::create([
             'name' => 'Admin User',
             'email' => 'admin@example.com',
-            'password' => Hash::make('password'),
+            'password' => Hash::make($adminPassword),
             'email_verified_at' => now(),
         ]);
-        $adminUser->assignRole('admin');
 
-        // $this->createTeamForUser($adminUser);
-    }
+        $team = Team::firstOrFail();
+        $adminUser->teams()->syncWithoutDetaching([$team->id]);
 
-    private function createTeamForUser($user)
-    {
-        $team = Team::first();
-        $team->users()->attach($user);
-        $user->current_team_id = 1;
-        $user->save();
+        $role = Role::where('name', 'super_admin')->firstOrFail();
+        $adminUser->assignRole($role);
+
+        // Print passwords to console
+        echo "Admin password: {$adminPassword}\n";
     }
 }
