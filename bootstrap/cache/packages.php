11dd04ac61e0f3993c1035b18fe0483c0a56fc87
DeveloperZONE <tdrabik@developerzone.eu>
2026-06-28 16:26:01 +0100

    chore: update Composer dependencies
    Bump minimum version constraints to current releases across Filament,
    Laravel (framework/horizon/jetstream/octane/reverb/sanctum/tinker),
    Livewire, Shield, Spatie packages, RoadRunner, PHPUnit and dev tooling.
    Regenerate bootstrap/cache package + service manifests.

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -165,13 +165,6 @@
       0 => 'Laravel\\Pail\\PailServiceProvider',
     ),
   ),
-  'laravel/passkeys' => 
-  array (
-    'providers' => 
-    array (
-      0 => 'Laravel\\Passkeys\\PasskeysServiceProvider',
-    ),
-  ),
   'laravel/reverb' => 
   array (
     'providers' => 
